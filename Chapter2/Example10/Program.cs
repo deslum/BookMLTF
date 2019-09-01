a0e6b938518355772253a0a5f6254aaad44769b2
deslum <yurij-bukatkin@yandex.ru>
2019-09-01 18:02:33 +0300

    First examples

---
+++ Chapter2/Example10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static Tensorflow.Binding;
+using NumSharp;
+
+namespace Example10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rawData = np.random.normal(10, 1, 100);
+
+            var alpha = tf.constant(0.05f);
+
+            var currValue = tf.placeholder(tf.float32);
+
+            var prevAvg = tf.Variable(0f);
+
+            var updateAvg = alpha * currValue + (1f - alpha) * prevAvg;
+
+            var init = tf.global_variables_initializer();
+
+            using (var sess = tf.Session())
+            {
+                sess.run(init);
+
+                foreach (var data in rawData)
+                {
+                    var currAvg = sess.run(updateAvg, (currValue, Convert.ToSingle(data)));
+                    sess.run(prevAvg.assign(currAvg));
+                    Console.WriteLine($"CurrentValue {data} CurrentAverage {currAvg}");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter2/Example8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static Tensorflow.Binding;
+
+
+namespace Example8
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = new [,]{{1f,2f}};
+            var x = tf.constant(a);
+            var negativeMatrix = tf.negative(x);
+            
+            using (var sess  = tf.Session())
+            {
+                var result = sess.run(negativeMatrix);
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
